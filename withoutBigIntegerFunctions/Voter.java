813894dd4719ae4a9d173e675d6d01100b6ce9fe
ikabar <ikabar@b7106390-1f54-11de-a7a1-25a91d9625e5>
2009-04-18 13:48:41 +0000

    VotingSystem updated, logger added.

--- withoutBigIntegerFunctions/Voter.java
+++ withoutBigIntegerFunctions/Voter.java
@@ -1,16 +1,35 @@
+import java.math.BigInteger;
 import java.util.Random;
 
-
 public class Voter {
-	private boolean vote;
+	private static int counter = 0;
+	private int vote;
+	private int voterID;
+	PublicKey publicKey;
 	
-	public Voter(){
-		Random random = new Random();
-		vote = random.nextBoolean();
+	/**
+	 * Voter Constructor <br>
+	 * the constructor do the following things:<br>
+	 * 1. sets the public key.<br>
+	 * 2. randomly choose a secret vote between 0 and 1.<br>
+	 * 3. log his choice in the logger.<br>
+	 * @param _publicKey, a public key that the voter use in order to send his vote.
+	 */
+	public Voter(PublicKey _publicKey){
+		this.voterID=counter; // initialization voter id
+		counter++;
+		this.publicKey = _publicKey; //setting public key
+		vote = new Random().nextInt(2); //generating vote between 0 and 1
+		VotingSystem.logger.info("Voter number "+voterID+" has been created. and voted for: "+vote);
 	}
 
-	public final boolean getVote() {
-		return vote;
+	/**
+	 * @return the voter choice encrypted with the public key.
+	 */
+	public final BigInteger getVote() {
+		BigInteger c = publicKey.encode(new BigInteger(Integer.toString(vote)));
+		VotingSystem.logger.info("Voter nubmer "+voterID+" has sent his incrypted vote: "+c.toString());
+		return c;
 	}
 	
 	
--- withoutBigIntegerFunctions/VotingSystem.java
+++ withoutBigIntegerFunctions/VotingSystem.java
@@ -1,4 +1,56 @@
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class VotingSystem {
+	
+	public static Logger logger = Logger.getLogger("CryptoLogger");
+	
+	
+	
+	public static void main(String[] args) throws SecurityException, IOException {
+		BigInteger allVotes; 
+		
+//		logger.addHandler(new FileHandler("unreal.xml"));
+    	logger.setLevel(Level.FINE);	
+    	
+    	//creating keys.
+    	Key [] keys= MyPaillier.generateKey();
+    	PrivateKey privateKey = (PrivateKey) keys[0];
+    	PublicKey publicKey = (PublicKey) keys[1];
+    	
+    	logger.info("Stating Voting System");
+    	Scanner reader = new Scanner(System.in);
+    	System.out.println("Please insert the amount of voters you want: ");
+    	int amountOfVoters = reader.nextInt();
+    	logger.info("The amount of voters is: "+amountOfVoters);
+    	Voter [] kalpi = new Voter[amountOfVoters];
+    	for (int i = 0; i < kalpi.length; i++) {//voting
+			kalpi[i] = new Voter(publicKey);
+		}
+    	
+    	//submitting the votes (encrypted of course)
+    	allVotes = kalpi[0].getVote();//assuming there is one voter minimum
+    	for (int i = 1; i < kalpi.length; i++) {//voting
+			allVotes = allVotes.multiply(kalpi[i].getVote());
+		}
+    	
+    	//now allVotes have the entire votes encrypted
+    	
+    	System.out.println("Voting result");
+    	BigInteger result = privateKey.decode(allVotes);
+    	System.out.println("The amount of pepole that think that the egg came before the chicken is: "+result.toString());
+    	System.out.println("The amount of pepole that think that the chicken came before the egg is: "+(amountOfVoters-result.intValue()));
+    	
+    	
+    	
+		
+//		Voter [] kalpi = new Voter();
+		
+	}
 
 }
